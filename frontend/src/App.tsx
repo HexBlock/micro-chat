a090ab0566fbbaa5f6822b73189d7c9c3c62ea45
Adam Ż <azelazows@gmail.com>
2022-02-09 20:33:53 +0100

    Initially added react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,21 @@
+import { Route, Link, Routes } from 'react-router-dom';
+import { Login } from './app/sites/login/Login';
+import { Register } from './app/sites/register/Register';
+
 export const App = () => {
-  return <div className="App">null</div>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Login</Link> | <Link to="register">Register</Link>
+      </nav>
+
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />}>
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
+  );
 };
