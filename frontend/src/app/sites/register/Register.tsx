23c3d8fde5512f05ec93fd173b0705818c0e8db4
Adam Ż <azelazows@gmail.com>
2022-02-12 16:58:26 +0100

    Added register page

--- frontend/src/app/sites/register/Register.tsx
+++ frontend/src/app/sites/register/Register.tsx
@@ -1,3 +1,47 @@
+import { Link } from 'react-router-dom';
+import styles from './register.module.scss';
+
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles['register-page']}>
+      <div className={`${styles['register-page__content']} common-card`}>
+        <h1>Register</h1>
+
+        <form autoComplete="off">
+          <div className="mt-3">
+            <label htmlFor="register-id" className="form-label">
+              Login
+            </label>
+            <input id="register-id" type="text" className="form-control" />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="pass-id" className="form-label">
+              Password
+            </label>
+            <input id="pass-id" type="password" className="form-control" />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="confirm-pass-id" className="form-label">
+              Confirm password
+            </label>
+            <input
+              id="confirm-pass-id"
+              type="password"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mt-5 d-flex align-items-center justify-content-between">
+            <button type="submit" className="btn mc-button mc-button-solid">
+              submit
+            </button>
+
+            <Link to="/" className="mc-anchor">
+              (return to login)
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
