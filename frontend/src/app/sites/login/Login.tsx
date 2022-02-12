bb53f22c77aaf2ddc33ba2967da7ea220b3233aa
Adam Ż <azelazows@gmail.com>
2022-02-12 16:45:26 +0100

    Added login page and style encapsulation

--- frontend/src/app/sites/login/Login.tsx
+++ frontend/src/app/sites/login/Login.tsx
@@ -1,3 +1,35 @@
+import { Link } from 'react-router-dom';
+import styles from './login.module.scss';
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles['login-page']}>
+      <div className={`${styles['login-page__content']} common-card`}>
+        <h1>Hello!</h1>
+
+        <form autoComplete="off">
+          <div className="mt-3">
+            <label htmlFor="login-id" className="form-label">
+              Login
+            </label>
+            <input id="login-id" type="text" className="form-control" />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="pass-id" className="form-label">
+              Password
+            </label>
+            <input id="pass-id" type="password" className="form-control" />
+          </div>
+
+          <div className="mt-5 d-flex align-items-center justify-content-between">
+            <button type="submit" className="btn mc-button mc-button-flow">
+              sign in
+            </button>
+
+            <Link to="register" className="mc-anchor">sign up</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
