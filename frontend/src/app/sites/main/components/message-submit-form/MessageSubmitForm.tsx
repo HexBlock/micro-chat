e9e317aa6ce87966f77a1133a29b0ba91f7d3a70
Adam Ż <azelazows@gmail.com>
2022-02-17 20:58:15 +0100

    Some initial message submitter functions

--- frontend/src/app/sites/main/components/message-submit-form/MessageSubmitForm.tsx
+++ frontend/src/app/sites/main/components/message-submit-form/MessageSubmitForm.tsx
@@ -1,11 +1,44 @@
+import React, { useRef, useState } from 'react';
+
 export const MessageSubmitForm = () => {
+  const [message, setMessage] = useState('');
+  const formElement = useRef<HTMLFormElement>(null);
+
+  const handleMessageEnter = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      formElement.current?.dispatchEvent(
+        new Event('submit', { cancelable: true, bubbles: true })
+      );
+    }
+  };
+  const handleSubmitNewMessage = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('new message is:', {
+      message
+    });
+  };
+
   return (
-    <form autoComplete="off">
+    <form
+      ref={formElement}
+      autoComplete="off"
+      onSubmit={handleSubmitNewMessage}
+    >
       <div className="mt-1">
-        <label htmlFor="message-id" className="form-label">
+        <label htmlFor="new-message" className="form-label">
           Message
         </label>
-        <textarea id="message-id" rows={2} className="form-control"></textarea>
+        <textarea
+          id="new-message"
+          name="new-message"
+          rows={2}
+          className="form-control"
+          value={message}
+          onKeyDown={handleMessageEnter}
+          onInput={event => setMessage((event.target as HTMLTextAreaElement).value)}
+        ></textarea>
       </div>
 
       <div className="mt-1 d-flex align-items-center justify-content-end">
